a37a54056277b3fb8080dc253f34b8a8364f00c4
Sathiya@1998 <sathiyavenkatachalam258@gmail.com>
2024-07-05 10:44:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+const { MONGODB_URI } = require('./utils/config');
+const app = require('./app');
+
+// 1. connect to the database
+const mongoose = require('mongoose');
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB...');
+
+        // 2. start the server
+        app.listen(3001, () => {
+            console.log('Server is running on port 3001');
+        });
+    })
+    .catch(err => console.error('Could not connect to MongoDB...', err));
